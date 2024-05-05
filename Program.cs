bcdf73fee7be6979121e4bd7b03334ea5dfacf7a
Daniel Jary <danieljary41@gmail.com>
2024-05-05 18:39:55 -0300

    Projeto Concluido

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Guaruja");
+Console.WriteLine("Santos");
+Console.WriteLine("São Vicente");
+Console.WriteLine("Praia Grande");
+Console.WriteLine("Mongagua");
+Console.WriteLine("Ithanhaem");
+Console.WriteLine("Peruibe");
